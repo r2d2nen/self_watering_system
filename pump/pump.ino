49496aff3f769de3bd06edbc8201bbbb4f0890d3
Gullik <guIIik@users.noreply.github.com>
2017-06-29 16:01:56 +0200

    First verson of self watering systen with moisture sensor. The parameters are mere guesses though...

--- pump/pump.ino
+++ pump/pump.ino
@@ -1,25 +1,58 @@
 
-int pumpTime = 6000;
-int waitTime = 5000; // 1000*3600*24 - pumpTime
-int outPin = 2;  
-int led = LED_BUILTIN;
+int pulseHighTime = 150; // Set motor pin high for this amount of time.
+int pulseLowTime = 100; // Time between each pulse.
+int roundTimeDelay = 2000; // Time between each round.
+int wateringDelay = 5000; // Time between each moisture value check.
+
+int pin_pump = 2; // Turns the pump on.
+int pin_led = LED_BUILTIN; // Signals when the pump is running.
+int pin_sensor_activate = 3; // Turns the moisture on. It is only powered when needed to slowen oxidation.
+int pin_sensor_read = 7; // Reads the moisture value from the sensor.
+
+int moisture_tolerance = 500; // Water the plant when the moisture sensor value i higher than this value (higher value = drier soil).
+int waterRounds = 5; // Each watering contains this many rounds, with roundTimeDelay between them.
+int waterPulsesPerRound = 5; // Water pulses per round.
 
 void setup() {
-    // put your setup code here, to run once:
-    
-    pinMode(outPin, OUTPUT);
-    pinMode(led, OUTPUT);
-
+    pinMode(pin_pump, OUTPUT);
+    pinMode(pin_led, OUTPUT);
+    pinMode(pin_sensor_activate, OUTPUT);
 }
 
 void loop() {
-    // put your main code here, to run repeatedly:
-    digitalWrite(outPin, HIGH);  // send current to start pump
-    digitalWrite(led, HIGH);  // Light led light
-    delay(pumpTime);  // Wiat for the pump to work
-    digitalWrite(outPin, LOW);  // stop the pump
-    digitalWrite(led, LOW); // turn off the led light
-    delay(waitTime);  // wait untill the pump should work again
-
-    
+  int moisture = measure_moisture(pin_sensor_activate, pin_sensor_read);
+  
+  if (moisture > moisture_tolerance) {
+    digitalWrite(pin_led, HIGH);
+    water(waterRounds, waterPulsesPerRound, roundTimeDelay, pin_pump, pulseHighTime, pulseLowTime);
+  }
+  else {
+    digitalWrite(pin_led, LOW);
+  }
+  delay(wateringDelay);
 }
+
+void waterPulse(int Ipin_pump, int IpulseHighTime, int IpulseLowTime) { // I = ''Input''
+  digitalWrite(Ipin_pump, HIGH);  // Send current to start pump.
+  delay(IpulseHighTime);  // Wait for the pump to work.
+  digitalWrite(Ipin_pump, LOW);  // Stop the pump.
+  delay(IpulseLowTime);  // Wait untill the pump should work again
+}
+
+void water(int IwaterRounds, int IwaterPulsesPerRound, int IroundTimeDelay, int Ipin_pump, int IpulseHighTime, int IpulseLowTime) {
+  for (int i = 0; i < IwaterRounds; i++) {
+    for (int j = 0; j < IwaterPulsesPerRound; j++) {
+        waterPulse(Ipin_pump, IpulseHighTime, IpulseLowTime);
+    }
+    delay(IroundTimeDelay);
+  }
+}
+
+int measure_moisture(int Ipin_sensor_activate, int Ipin_sensor_read) {
+  digitalWrite(Ipin_sensor_activate, HIGH);
+  delay(100);
+  int val = analogRead(Ipin_sensor_read);
+  digitalWrite(Ipin_sensor_activate, LOW);
+  return val;
+}
+
